1f47080df867949115f472accc661cd90012270d
Josue Valbuena <joshvlbn@gmail.com>
2023-06-12 21:37:49 -0400

    home page y local storage listo, resolver pagina carrito

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,19 +1,33 @@
-import { dataBase } from "./db.js"
-
 //import components from navbar
 const btnNavMenuList = document.querySelector("#categorias");
 const navMenuList = document.querySelector(".nav-menu-list");
+const shoppingCardQty = document.querySelector(".nav-menu-sc-span")
+
 //import html div to render producst
 const productRenderDiv = document.querySelector(".products")
+const productRenderShoppingCardDiv = document.querySelector(".products-shoppingCard")
 
 //import buttons from card in main page
 let buttonAddToCard = document.getElementsByClassName("card-shoppingCard-right-addButton");
 
+//import buttons from shopping card page
+const epmtycard = document.querySelector("#products-shoppingCard-emptyCard");
+
 btnNavMenuList.addEventListener("click", () => navMenuList.classList.toggle("visible"));
 
-//Rendering dataBase in HTML with a for
+//Import database
+const productos = './assets/js/db.json';
 
-function allProducts() {
+//Rendering dataBase in HTML with a for
+async function getData() {
+    const res = await fetch(productos);
+    const data = await res.json();
+    return data;
+}
+
+//Rendering all products in index.html
+async function renderProducts() {
+    const dataBase = await getData()
     dataBase.forEach(producto => {
         productRenderDiv.innerHTML += `<div class="card">
         <img src=${producto.img} alt="">
@@ -24,7 +38,7 @@ function allProducts() {
                 <div class="card-shoppingCard">
                     <p class="card-shoppingCard-price">$${producto.price}</p>
                     <div class="card-shoppingCard-right">
-                        <button class="card-shoppingCard-right-addButton" id=${producto.id}">Agregar al Carrito</button>
+                        <button class="card-shoppingCard-right-addButton" id=${producto.id}>Agregar al Carrito</button>
                         
                         <button class="card-shoppingCard-right-giveButton">-</button>
                         <input class="card-shoppingCard-right-input" type="number" min="0" value="1">
@@ -39,22 +53,58 @@ function allProducts() {
     buttonsFunctions();
 }
 
-allProducts();
+window.onload = renderProducts();
+
 
 //Adding function to buttons
-
 function buttonsFunctions() {
     buttonAddToCard = document.querySelectorAll(".card-shoppingCard-right-addButton");
     buttonAddToCard.forEach(ele => ele.addEventListener("click", addTocard));
 }
 
 //Shopping Card functions
+let shoppingCard;
+const shoppingCardStorage = JSON.parse(localStorage.getItem('SC'));
+if (shoppingCardStorage) {
+    shoppingCard = shoppingCardStorage;
+    shoppingCardQtyRender()
+} else {
+    shoppingCard = [];
+}
 
-const shoppingCard = [];
+//Rendering card products in Shopping Card .html
+function renderShoppingCardProducts() {
+    shoppingCard.forEach(products =>
+        productRenderShoppingCardDiv.innerHTML += `
+        <div>${products.title}</div>
+        `
+    )
+}
 
-function addTocard(e) {
+epmtycard.addEventListener("click", () => emptyCard());
+
+function shoppingCardQtyRender() {
+    shoppingCardQty.textContent = shoppingCard.map(ele => ele.qty).reduce((a, b) => a + b, 0);
+}
+
+async function addTocard(e) {
     const buttonId = e.currentTarget.id;
-    const productToAdd = dataBase.find(ele => ele.id === buttonId);
-    console.log(buttonId);
-    console.log(productToAdd);
+    const dataBase = await getData();
+    const existe = shoppingCard.some(ele => ele.id === Number(buttonId))
+    if (existe) {
+        const index = shoppingCard.findIndex(ele => ele.id === Number(buttonId))
+        shoppingCard[index].qty++;
+    } else {
+        const productToAdd = dataBase.find(ele => ele.id === Number(buttonId));
+        productToAdd.qty = 1;
+        shoppingCard.push(productToAdd);
+    }
+    localStorage.setItem('SC', JSON.stringify(shoppingCard));
+    shoppingCardQtyRender();
+}
+
+function emptyCard() {
+    shoppingCard = [];
+    localStorage.setItem("SC", JSON.stringify(shoppingCard));
+    shoppingCardQtyRender()
 }
